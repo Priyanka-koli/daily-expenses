c7513a9833605e7e19a2142eab51edc746cec236
Priyanka-koli <kolipriyu11@gmail.com>
2022-08-26 13:14:16 +0530

    add form validations

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,14 +2,6 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  /* const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = "$294.67"; */
-  /* const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Hello");
-  };
-  console.log(title); */
   return (
     <div className="expense-item">
       <ExpenseDate data={props.date} />
@@ -17,7 +9,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      {/* <button onClick={clickHandler}>Change title</button> */}
     </div>
   );
 };
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,18 +4,38 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setTitle] = useState("");
   const [enteredAmount, setAmount] = useState("");
   const [enteredDate, setDate] = useState("");
+  const [isEnteredTitle, setIsEnteredTitle] = useState(true);
+  const [isEnteredAmount, setIsEnteredAmount] = useState(true);
+  const [isEnteredDate, setIsEnteredDate] = useState(true);
 
   const titleChangeHandler = (e) => {
+    if (e.target.value.trim()) {
+      setIsEnteredTitle(true);
+    }
     setTitle(e.target.value);
   };
   const amountChangeHandler = (e) => {
     setAmount(e.target.value);
+    setIsEnteredAmount(true);
   };
   const dateChangeHandler = (e) => {
     setDate(e.target.value);
+    setIsEnteredDate(true);
   };
   const submitHandler = (e) => {
     e.preventDefault();
+
+    enteredTitle.trim().length === 0 && setIsEnteredTitle(false);
+    enteredAmount.trim().length === 0 && setIsEnteredAmount(false);
+    enteredDate.trim().length === 0 && setIsEnteredDate(false);
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredAmount.trim().length === 0 ||
+      enteredDate.trim().length === 0
+    ) {
+      return;
+    }
 
     const ExpenseData = {
       title: enteredTitle,
@@ -36,40 +56,38 @@ const ExpenseForm = (props) => {
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <label>
-            Title
-            <input
-              type="text"
-              value={enteredTitle}
-              onChange={titleChangeHandler}
-            />
-          </label>
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+            className={`${!isEnteredTitle ? "invalid" : ""}`}
+          />
         </div>
 
         <div className="new-expense__control">
-          <label>
-            Amount
-            <input
-              type="number"
-              value={enteredAmount}
-              onChange={amountChangeHandler}
-              min="0.1"
-              step="0.01"
-            />
-          </label>
+          <label>Amount</label>
+          <input
+            type="number"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+            min="0.1"
+            step="0.01"
+            className={`${!isEnteredAmount ? "invalid" : ""}`}
+          />
         </div>
         <div className="new-expense__control">
-          <label>
-            Date
-            <input
-              type="date"
-              value={enteredDate}
-              min="2019-01-01"
-              max="2022-12-31"
-              onChange={dateChangeHandler}
-            />
-          </label>
+          <label>Date</label>
+          <input
+            type="date"
+            value={enteredDate}
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+            className={`${!isEnteredDate ? "invalid" : ""}`}
+          />
         </div>
+
         <div className="new-expense__actions">
           <button onClick={cancelClickHandler}>Cancel</button>
         </div>
@@ -77,6 +95,11 @@ const ExpenseForm = (props) => {
           <button type="submit">Add Expense</button>
         </div>
       </div>
+      {(!isEnteredAmount || !isEnteredDate || !isEnteredTitle) && (
+        <div className="form-error">
+          <p className="validation-error"> Please enter all values!!!</p>
+        </div>
+      )}
     </form>
   );
 };
